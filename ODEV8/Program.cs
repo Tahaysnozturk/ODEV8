410b2d403bf368d7ac3b2f6667f6805c53db6697
tiyol <tiyol@LAPTOP-6IPT3PBJ>
2024-01-07 00:26:32 +0300

    Proje dosyası ekle.

---
+++ ODEV8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Kaç adet sayı girmek istiyorsunuz? ");
+        int adet = Convert.ToInt32(Console.ReadLine());
+
+        int toplam = 0;
+
+        for (int i = 1; i <= adet; i++)
+        {
+            Console.Write("Sayı {0}: ", i);
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            toplam += sayi;
+        }
+
+        double ortalama = (double)toplam / adet;
+
+        Console.WriteLine("Girilen sayıların toplamı: " + toplam);
+        Console.WriteLine("Girilen sayıların ortalaması: " + ortalama);
+    }
+}
